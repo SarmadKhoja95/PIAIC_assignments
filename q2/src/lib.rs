0c53fa433be1b2c956b2a793ce2609e2478c7cda
SarmadKhoja95 <sarmadkhoja.sk@gmail.com>
2020-01-20 00:14:25 +0500

    Question#2

---
+++ q2/src/lib.rs
@@ -0,0 +1,10 @@
+#![allow(dead_code)]
+pub mod hospital{
+    pub mod patient{
+        pub fn wait_for_turn(){
+            println!("Wait for your turn");
+        }
+
+    }
+
+}
---
+++ q2/src/main.rs
@@ -0,0 +1,5 @@
+use q2::hospital;
+
+fn main() {
+    crate::hospital::patient::wait_for_turn();
+}
